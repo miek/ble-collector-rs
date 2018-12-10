cfc43b6d494e80f100d19870adac4ab01c27fdb3
Mike Walters <mike@flomp.net>
2018-12-10 15:47:42 +0000

    Punt data to influx

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,29 @@
 extern crate byteorder;
+#[macro_use] extern crate influx_db_client;
 #[macro_use] extern crate mac;
 extern crate rumble;
  
 use std::collections::HashMap;
 use std::io::Cursor;
 use std::thread;
-use std::time::Duration;
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
+
 use byteorder::{BigEndian, LittleEndian, ReadBytesExt};
+use influx_db_client::{Client, Point, Points, Value, Precision};
 use rumble::bluez::manager::Manager;
 use rumble::api::{Central, Peripheral};
 
 #[derive(Debug)]
 struct RuuviPacket {
     version: u32,
-    humidity: f32,
-    temperature: f32,
-    pressure: f32,
-    acceleration_x: f32,
-    acceleration_y: f32,
-    acceleration_z: f32,
-    voltage: f32,
+    mac: String,
+    humidity: f64,
+    temperature: f64,
+    pressure: f64,
+    acceleration_x: f64,
+    acceleration_y: f64,
+    acceleration_z: f64,
+    voltage: f64,
 }
 
 fn decode_ruuvi_format_3(buf: &mut std::io::Read) -> Option<RuuviPacket> {
@@ -36,23 +40,24 @@ fn decode_ruuvi_format_3(buf: &mut std::io::Read) -> Option<RuuviPacket> {
     let accel_z = unwrap_or_return!(buf.read_i16::<BigEndian>().ok(), None);
     let battery_mv = unwrap_or_return!(buf.read_u16::<BigEndian>().ok(), None);
 
-    let humidity = humidity as f32 / 2.0;
+    let humidity = humidity as f64 / 2.0;
     let sign = match temp_int & 0x80 == 0 {
         true => 1.0,
         false => -1.0,
     };
-    let temperature = (temp_int & !(0x80)) as f32 * sign + temp_hundredths as f32 / 100.0;
-    let pressure = pressure as f32 + 50000.0;
-    let voltage = battery_mv as f32 / 1000.0;
+    let temperature = (temp_int & !(0x80)) as f64 * sign + temp_hundredths as f64 / 100.0;
+    let pressure = pressure as f64 + 50000.0;
+    let voltage = battery_mv as f64 / 1000.0;
 
     Some(RuuviPacket{
         version: 3,
+        mac: String::new(),
         humidity: humidity,
         temperature: temperature,
         pressure: pressure,
-        acceleration_x: accel_x as f32,
-        acceleration_y: accel_y as f32,
-        acceleration_z: accel_z as f32,
+        acceleration_x: accel_x as f64,
+        acceleration_y: accel_y as f64,
+        acceleration_z: accel_z as f64,
         voltage: voltage,
     })
 }
@@ -63,6 +68,23 @@ fn decode_ruuvi_packet(packet: &Vec<u8>) -> Option<RuuviPacket> {
         return None;
     }
     [decode_ruuvi_format_3].iter().find_map(|f| f(&mut buf.clone()))
+}
+
+fn influx_post(pkt: &RuuviPacket) -> () {
+    let client = Client::new("http://localhost:8086", "ruuvi");
+    let mut point = point!("ruuvi_measurements");
+    point.add_timestamp(SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs() as i64);
+    point.add_field("humidity", Value::Float(pkt.humidity));
+    point.add_field("temperature", Value::Float(pkt.temperature));
+    point.add_field("pressure", Value::Float(pkt.pressure));
+    point.add_field("acceleration_x", Value::Float(pkt.acceleration_x));
+    point.add_field("acceleration_y", Value::Float(pkt.acceleration_y));
+    point.add_field("acceleration_z", Value::Float(pkt.acceleration_z));
+    point.add_field("voltage", Value::Float(pkt.voltage));
+    point.add_tag("mac", Value::String(pkt.mac.clone()));
+
+    let points = points!(point);
+    let _ = client.write_points(points, Some(Precision::Seconds), None).unwrap();
 }
  
 pub fn main() {
@@ -93,8 +115,9 @@ pub fn main() {
             };
             let ls = last_seen.entry(prop.address).or_insert(0);
             if *ls != prop.discovery_count {
-                if let Some(p) = ruuvi_packet {
-                    println!("{:?}", p);
+                if let Some(mut p) = ruuvi_packet {
+                    p.mac = format!("{}", prop.address);
+                    influx_post(&p);
                 }
             }
             *ls = prop.discovery_count;
